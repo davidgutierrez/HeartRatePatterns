43096a3c879830fbefb579b3963e570f8f2cff40
David Gutierrez <cgutierrez@fconsultor.com>
2017-08-16 15:31:27 -0500

    Update DataLoad.py
    Downloads Only the data of the Signal II if it exist.

--- Python/DataLoad.py
+++ Python/DataLoad.py
@@ -2,12 +2,14 @@
 import urllib.request  # the lib that handles the url stuff
 import wfdb # the lib that handles the physionet stuff
 import numpy # the lib that handles the arrays stuff
+import gc
 from scidbpy import connect #the lib that handles the scidby stuff
 from sklearn.preprocessing import normalize
 
 def uploadToSDB(sigII,ondaName,sdb):
   array = numpy.asarray(sigII, dtype=float)
   arrayNonNan = array[~numpy.isnan(array)]
+  arrayNonNan = numpy.trim_zeros(arrayNonNan)
   if arrayNonNan.any():
     print("uploading: "+ondaName)
     array = normalize(arrayNonNan[:,numpy.newaxis], axis=0).ravel()
@@ -17,12 +19,14 @@ def uploadToSDB(sigII,ondaName,sdb):
 def downloadWFDB(onda,carpeta,sdb,ondaName):
   signalII = None
   try:
-    sig, fields = wfdb.srdsamp(onda,pbdir='mimic2wdb/matched/'+carpeta)
-    signalII = fields['signame'].index("II")
+    sig, fields = wfdb.srdsamp(onda,pbdir='mimic2wdb/matched/'+carpeta, sampto=1)
+    channel = fields['signame'].index("II")
+    if(channel!=None):
+      sig, fields = wfdb.srdsamp(onda,pbdir='mimic2wdb/matched/'+carpeta, channels = [channel])
+      print("downloaded:" + ondaName)
+      uploadToSDB(sig[:,0],ondaName,sdb)
   except ValueError:
     print(ondaName+" no contains Signal II")
-  if(signalII!=None):
-    uploadToSDB(sig[:,signalII],ondaName,sdb)
   return;
 
 def fillReadedWaves(sdb):
@@ -38,6 +42,7 @@ data = urllib.request.urlopen(target_url) # it's a file like object and works ju
 sdb = connect('http://localhost:8080') #the url of the scidb service
 sdbarrays = fillReadedWaves(sdb)
 for line in data: # files are iterable
+    gc.collect()
     file = open("Jupyter/readedWaves.txt","a") 
     carpeta,onda = str(line).replace('b\'','').replace('\'','').replace('\\n','').split("/")
     ondaName = onda.replace("-", "_")
