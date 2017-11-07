4128bcc678b8c7e0ff65473cb93bd528f500ec4a
David Gutierrez <cgutierrez@fconsultor.com>
2017-11-06 22:25:22 -0500

    Update LogisticRegresion.py

--- Python/LogisticRegresion.py
+++ Python/LogisticRegresion.py
@@ -25,6 +25,12 @@ def find_new_explore_c(paramArray, best_val):
             paramArray = np.linspace(paramArray[index_val - 1]+distance_prev/2, max_val, len(paramArray)).tolist()
     return paramArray
 
+def logisticRegression(C,solver,model_train,objetive_train,objetive_test,model_test):
+    model = LogisticRegression(C=best_val[0],fit_intercept=True, penalty='l2',dual=True,solver=solver,verbose=0,random_state=0)
+    model.fit(model_train,objetive_train)
+    acscoreb = accuracy_score(objetive_test,model.predict(model_test))
+    return model,acscoreb
+
 def ajustLogisticRegression(model_train,objetive_train,model_test,objetive_test,exploreC=[0.0001,0.001,0.01,0.1,1,10]) :
     best_avg=0
     solver='liblinear'
@@ -39,26 +45,20 @@ def ajustLogisticRegression(model_train,objetive_train,model_test,objetive_test,
         best_val=logitmodel.C_
         best_average = np.average(scores,0)[exploreC.index(best_val)]
 #        print("CV averages for values "+str(exploreC)+" are:"+str(np.average(scores,0)))
-        print("Best C is "+str(best_val)+" with an average of "+str(best_average))
+#        print("Best C is "+str(best_val)+" with an average of "+str(best_average))
         if best_avg<best_average :
           bestC = best_val[0]
           best_avg=best_average
         exploreC=find_new_explore_c(exploreC, best_val)
     #return a logisticRegression vased in the best_val and the parameter used
     acscore = accuracy_score(objetive_test,logitmodel.predict(model_test))
-    print("Score logit "+str(acscore))
-
-    model = LogisticRegression(C=best_val[0],fit_intercept=True, penalty='l2',dual=True,solver=solver,verbose=0,random_state=0)
-    model.fit(model_train,objetive_train)
-    acscoreb = accuracy_score(objetive_test,model.predict(model_test))
-    print("Score last "+str(acscoreb))
+#    print("Score logit "+str(acscore))
+    model,acscoreb = logisticRegression(best_val[0],solver,model_train,objetive_train,objetive_test,model_test)
+#    print("Score last "+str(acscoreb))
     acscore = acscoreb if acscoreb>acscore else acscore
     model = model if acscoreb>acscore else logitmodel
-
-    modelC = LogisticRegression(C=bestC      ,fit_intercept=True, penalty='l2',dual=True,solver=solver,verbose=0,random_state=0)
-    modelC.fit(model_train,objetive_train)
-    acscorec = accuracy_score(objetive_test,modelC.predict(model_test))
-    print("Score best "+str(acscorec))
+    modelC,acscorec = logisticRegression(bestC,solver,model_train,objetive_train,objetive_test,model_test)
+#    print("Score best "+str(acscorec))
     acscore = acscore if acscore>acscorec else acscorec
     model = modelC if acscorec>acscoreb else model
     print("acurracy is %2.2f" % acscore)
